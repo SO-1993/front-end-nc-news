78e62e7805981b883992ec191bb93099db74cf1e
Scott <scottobrien93@outlook.com>
2024-11-13 15:28:16 +0000

    User can view a list of all articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from "react";
-import Header from "../Header";
-import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+
+import ArticleList from "./components/ArticleList";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<ArticleList />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { fetchArticles } from "../../utils/api";
+import { Link } from "react-router-dom";
+
+function ArticleList() {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    fetchArticles()
+      .then((articles) => {
+        setArticles(articles);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
+
+  if (error) return <p>There has been an error!!!</p>;
+  if (loading) return <p>Loading articles...</p>;
+  if (articles.length === 0) return <p>No articles available.</p>;
+
+  return (
+    <section>
+      {articles.map((article) => (
+        <div key={article.article_id}>
+          <Link to={`/articles/${article.article_id}`}>
+            <h2>{article.title}</h2>
+          </Link>
+        </div>
+      ))}
+    </section>
+  );
+}
+
+export default ArticleList;
